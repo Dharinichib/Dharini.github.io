08ddf92de0aa752a7778e14edc0770c6d48853eb
Dharinichib <127197788+Dharinichib@users.noreply.github.com>
2023-03-23 14:20:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,39 @@
 
-function myfunction()
-{  
-    var a = document.getElementById("n1").value,
-        b = document.getElementById("n2").value,
-        c = document.getElementById("n3").value,
-        d = document.getElementById("n4").value,
-        e = document.getElementById("n5").value,
-        f = form1.gender.value,
-        g = document.getElementsByName("course");
-        var result = " ";
-  
-        for (var i = 0; i < g.length; i++) {
-            if (g[i].checked) {
-                result += g[i].value+" ";
-    }
-  }
-    document.write("<h1>SUBMITTED FORM DETAILS:</h1>")
-    document.write("<br>FirstName: ",a);
-    document.writeln("<br>LastName: ",b);
-    document.write("<br>E-Mail: ",c);
-    document.write("<br>Mobile Number: ",d);
-    document.write("<br>State: ",e);
-    document.write("<br>Gender: ",f);
+const submitBtn= document.querySelector('#submit');
+const form=document.querySelector('#form1');
+//var displayButton = document.getElementById("name") 
+
+submitBtn.addEventListener('click',(e)=>{
+    e.preventDefault();
+    const formData=new FormData(form);
+    const values=[...formData.entries()];
+   
+    var obj=Object.fromEntries(values);
+    //console.log(obj);
+    let user1 = getData() ?? [];
+    const array = [...user1];
+    array.push(obj);
+    console.log(array)
+    localStorage.setItem('userdata',JSON.stringify(array));
     
-       
-      document.write("<br>Language: "
-                    + result);
-    }
+});
+function getData(){
+    let user = JSON.parse(localStorage.getItem('userdata'));
+    return user;
+   // console.log(user);
+   }
+   
+ document.getElementById("btnn").addEventListener("click",function(){
+  //  console.log('data')
+    let data = localStorage.getItem("userdata");
+    data = JSON.parse(data)
+    console.log(data);
+    result.innerHTML = '<h3 class="user-detail">User Details</h3>'
+    data.map((item, idx) => {
+       result.innerHTML += `<p class="user-title">User ${idx+1}</p>`;
+       for (const key in item) {
+          result.innerHTML += `<p>${key} : ${item[key]}</p>`;
+       }
+    })
+})
+l
